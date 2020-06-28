f04636a79fd71c52d183b88e4abfa2aba51e1181
Octav <octavianortari@gmail.com>
2020-06-28 11:58:41 +0100

    adds insertionSort

--- algorithms_you_should_know/Sorter.py
+++ algorithms_you_should_know/Sorter.py
@@ -1,7 +1,31 @@
 class Sorter():
+
 	""" Sorter class is a collection of the most popular 
         sorting algorithms that you should know
 
 	"""
 	def __init__(self, array):
-              print(array)
+		self.array = array
+
+	def insertionSort(self):
+		"""Function to sort an array using insertion sort
+		algorithm.
+		
+		Args: 
+			None
+		
+		Returns: 
+			list: sorted array 
+
+		"""
+		for i in range(1, len(self.array)):
+			value = self.array[i]
+			j = i
+
+			while j > 0 and self.array[j - 1] > value:
+				self.array[j] = self.array[j - 1]
+				j = j - 1
+
+			self.array[j] = value
+
+		return self.array
--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 import unittest
 
-from algorithms import Sorter
+from algorithms_you_should_know import Sorter
 
 class TestSorterClass(unittest.TestCase):
     def setUp(self):
-        self.sorter = Sorter([2, 1, 3])
+        self.sorter = Sorter([2, 1, 3, 5, 7, 4, 6])
 
     def test_initialization(self): 
-        self.assertEqual(self.sorter.insertionSort(), [1, 2, 3], 'incorrect sorted order')
+        self.assertEqual(self.sorter.insertionSort(), [1, 2, 3, 4, 5, 6, 7], 'incorrect sorted order')
+
+if __name__ == '__main__':
+    unittest.main()
